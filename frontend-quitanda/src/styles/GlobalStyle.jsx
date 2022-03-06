43ba8a60291bc4d745084ab691b2435b56feba46
BrenerQuevedo <brener.quevedo@ccc.ufcg.edu.br>
2022-03-06 18:41:39 -0300

    globalstyle and theme provider

---
+++ frontend-quitanda/src/styles/GlobalStyle.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import wallpaper from "../assets/wallpaper.png";
+
+const GlobalStyle = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: 'Mulish', sans-serif;
+	}
+
+	html, body, #root {
+		height: 100%;
+		background: url(${wallpaper}) no-repeat center center fixed; 
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+	}
+	*, button, input {
+		border: 0;
+		outline: 0;
+		font-family: 'Mulish', sans-serif;
+	}
+	button {
+		cursor: pointer;
+	}
+`;
+
+const GlobalStyleComposed = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+    </React.Fragment>
+  );
+};
+
+export default GlobalStyleComposed;
