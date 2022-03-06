59aac2cb21a741aac8f4938ee7efd7c7f93bd985
BrenerQuevedo <brener.quevedo@ccc.ufcg.edu.br>
2022-03-06 18:40:27 -0300

    init pages

---
+++ frontend-quitanda/src/pages/Profile/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Profile = () => {
+  return <h1>PERFIL</h1>;
+};
+
+export default Profile;
---
+++ frontend-quitanda/src/pages/UserList/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const UserList = () => {
+  return <h1>UserList</h1>;
+};
+
+export default UserList;
