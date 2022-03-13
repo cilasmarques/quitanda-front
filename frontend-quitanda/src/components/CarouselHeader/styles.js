e2151ad83104ec2a0ac1b1c696627f2c67f2eea2
BrenerQuevedo <brener.quevedo@ccc.ufcg.edu.br>
2022-03-13 01:40:56 -0300

    CarouselHeader component for Carousel

---
+++ frontend-quitanda/src/components/CarouselHeader/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  background: ${(props) => props.theme.colors.primary.container};
+  border-radius: 0.3rem;
+`;
+
+export const TitleContainer = styled.div`
+  h3 {
+    font-family: "Mulish";
+    font-style: normal;
+    font-weight: bold;
+    font-size: 1.25rem;
+    line-height: 1.875rem;
+    letter-spacing: 0.3px;
+  }
+`;
+
+export const Controllers = styled.div`
+  display: flex;
+  align-items: center;
+  width: 150px;
+  justify-content: end;
+`;
