6fe4580abdf7c4a1f8c1fc7c35bcee3bc65d9cca
Cilas Marques <cilas.marques@ccc.ufcg.edu.br>
2022-03-20 15:37:11 -0300

    Update routes

--- frontend-quitanda/src/routes/AuthRoutes/router.jsx
+++ frontend-quitanda/src/routes/AuthRoutes/router.jsx
@@ -3,13 +3,15 @@ import { Routes as Switch, Route } from 'react-router-dom';
 // COMPONENTS
 import SignInPage  from '../../pages/SignIn';
 import SignUpPage from '../../pages/SignUp';
+import Products from '../../pages/Products';
 import NotFound from '../../pages/NotFound';
 
 export const AuthRoutes = () => {
 	return (
 		<Switch>
 			<Route path="/" element={<SignInPage />} />
-			<Route path="/signup" element={<SignUpPage />} />
+			<Route path="/cadastro" element={<SignUpPage />} />
+      <Route path="/produtos" element={<Products />} />
 			<Route path="*" element={<NotFound />} />
 		</Switch>
 	);
