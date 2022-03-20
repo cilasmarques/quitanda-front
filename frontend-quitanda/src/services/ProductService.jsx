e0409f9fbc57dac3ae0f5b501ac79c1c5f310dfa
Cilas Marques <cilas.marques@ccc.ufcg.edu.br>
2022-03-20 15:34:30 -0300

    Update some routes and create de product service

---
+++ frontend-quitanda/src/services/ProductService.jsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { handleError } from '../utils/handleErrors';
+
+const url = 'http://localhost:8000';
+
+export async function addProduct(productList) {
+  try {
+    return await axios.post(`${url}/products`, {'product_list': productList});
+  } catch (error) {
+    handleError(error);
+  };
+}
+
+export async function getAllProducts(sortConfig, page) {
+  try {
+    return await axios.get(`${url}/products/${sortConfig}/${page}`);
+  } catch (error) {
+    handleError(error);
+  };
+};
+
+export async function getProductByName(productName) {
+  try {
+    return await axios.get(`${url}/product/${productName}`);
+  } catch (error) {
+    handleError(error);
+  };
+};
+
+export async function getProductById(productId) {
+  try {
+    return await axios.get(`${url}/product/${productId}`)
+  } catch (error) {
+    handleError(error);
+  };
+};
+
+export async function updateProduct(data) {
+  try {
+    return await axios.put(`${url}/product`, data,
+      // headers: { 'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`},
+    );
+  } catch (error) {
+    handleError(error);
+  };
+};
+
+export async function deleteProductById(productId) {
+  try {
+    return await axios.delete(`${url}/productid/${productId}`,
+      // headers: { 'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`},
+    );
+  } catch (error) {
+    handleError(error);
+  };
+};
--- frontend-quitanda/src/services/UserService.jsx
+++ frontend-quitanda/src/services/UserService.jsx
@@ -1,69 +1,35 @@
 import axios from 'axios';
-// import Cookies from 'js-cookie'
 import { handleError } from '../utils/handleErrors';
 
 const url = 'http://localhost:8000';
 
 export async function addUser(user) {
   try {
-    return await axios.post(`${url}/users`, user,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-      })
+    return await axios.post(`${url}/users`, user);
   } catch (error) {
     handleError(error);
   };
 }
 
-export async function getAllUsers(sort_config, page) {
+export async function getAllUsers(sortConfig, page) {
   try {
-    return await axios.get(`${url}/users`,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-        body: {
-          "sort_config": sort_config,
-          "page": page
-        },
-      })
+    return await axios.get(`${url}/users/${sortConfig}/${page}`);
   } catch (error) {
     handleError(error);
   };
 };
 
-export async function getAllValidUsers(sort_config, page) {
+export async function getAllValidUsers(sortConfig, page) {
   try {
-    return await axios.get(`${url}/valid_users`,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-        body: {
-          "sort_config": sort_config,
-          "page": page
-        },
-      })
+    return await axios.get(`${url}/valid_users/${sortConfig}/${page}`);
   } catch (error) {
     handleError(error);
   };
 };
 
-
-export async function getAllInvalidUsers(sort_config, page) {
+export async function getAllInvalidUsers(sortConfig, page) {
   try {
-    return await axios.get(`${url}/invalid_users`,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-        body: {
-          "sort_config": sort_config,
-          "page": page
-        },
-      })
+    return await axios.get(`${url}/invalid_users/${sortConfig}/${page}`);
   } catch (error) {
     handleError(error);
   };
@@ -71,12 +37,7 @@ export async function getAllInvalidUsers(sort_config, page) {
 
 export async function getUserByUsername(userName) {
   try {
-    return await axios.get(`${url}/user/${userName}`,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-      })
+    return await axios.get(`${url}/user/${userName}`);
   } catch (error) {
     handleError(error);
   };
@@ -84,15 +45,21 @@ export async function getUserByUsername(userName) {
 
 export async function updateUserByUsername(userName, userData) {
   try {
-    return await axios.put(`${url}/user/${userName}`, userData,
-      {
-        // headers: {
-        //   'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}`
-        // },
-      })
+    return await axios.put(`${url}/user/${userName}`, {
+      // headers: { 'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}` },
+      body: userData,
+    });
   } catch (error) {
     handleError(error);
   };
 };
 
-// TODO - implementar o delete > esperando o backend ficar pronto
+export async function deleteUserByUsername(userName) {
+  try {
+    return await axios.delete(`${url}/user/${userName}`, {
+      // headers: { 'Authorization': `Bearer ${localStorage.getItem(LocalStorageKeys.TOKEN)}` },
+    });
+  } catch (error) {
+    handleError(error);
+  };
+};
